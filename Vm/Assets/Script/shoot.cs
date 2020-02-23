a67c64d751e81fb07e837004757d7a225ecdc8ea
fabian-nunes <54534495+fabian-nunes@users.noreply.github.com>
2020-02-23 09:32:25 +0000

    Demo V1.3
    Versão atual. Vou criar ainda uma mini documentação

---
+++ Vm/Assets/Script/shoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimatorC;
+
+    private bool test = true;
+    
+    // Start is called before the first frame update
+    GameObject particle;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray))
+                {
+                    if (test == false)
+                    {
+                         myAnimatorC.SetBool("Shoot", true);
+                         test = true;
+                    }
+                    else
+                    {
+                        myAnimatorC.SetBool("Shoot", false);
+                        test = false;
+                    }
+                   
+                }
+            }
+        }
+    }
+}

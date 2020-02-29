36510cd517e6efc26b3fc3322b3c7c3bc9579241
fabian-nunes <54534495+fabian-nunes@users.noreply.github.com>
2020-02-29 12:23:51 +0000

    Mat ex 0.1

---
+++ Augmented_Learning/Assets/Fabian/Scripts/CorrectAnswer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectAnswer : MonoBehaviour
+{
+    public GameObject correct;
+
+    public GameObject text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (correct.name.Equals("co"))
+        {
+            text.SetActive(true);
+        }
+        else
+        {
+            
+        }
+    }
+
+    void Correct()
+    {
+        
+    }
+}

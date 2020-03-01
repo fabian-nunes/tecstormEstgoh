518ef59537e1a4e8d61c6413c794200fc63f76a8
fabian-nunes <54534495+fabian-nunes@users.noreply.github.com>
2020-03-01 01:51:47 +0000

    sound

---
+++ Augmented_Learning/Assets/Fabian/Scripts/touch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour
+{
+    private bool nice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nice = false;
+        this.GetComponent<AudioSource>().Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnMouseDown()
+    {
+        Debug.Log("nice");
+        if (nice == false)
+        {
+            this.GetComponent<AudioSource>().Play();
+            nice = true;
+        }
+        else
+        {
+            this.GetComponent<AudioSource>().Stop();
+            nice = false;
+        }
+    }
+}
